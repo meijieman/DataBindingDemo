0558f7c1a37792a35db69e184a51d8cc2e3a573e
freedomu <1072672770@qq.com>
2016-09-23 01:43:50 +0800

    POJO 继承 BaseObservable

--- app/src/main/java/com/hongfans/restfuldemo/vm/MainViewModel.java
+++ app/src/main/java/com/hongfans/restfuldemo/vm/MainViewModel.java
@@ -1,26 +1,35 @@
 package com.hongfans.restfuldemo.vm;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.android.databinding.library.baseAdapters.BR;
+
 /**
  * Created by Administrator on 2016/9/22.
  */
-public class MainViewModel {
+public class MainViewModel extends BaseObservable{
 
     private String text;
     private boolean pb;
 
+    @Bindable
     public String getText() {
         return text;
     }
 
     public void setText(String text) {
         this.text = text;
+        notifyPropertyChanged(BR.text);
     }
 
+    @Bindable
     public boolean isPb() {
         return pb;
     }
 
     public void setPb(boolean pb) {
         this.pb = pb;
+        notifyPropertyChanged(BR.pb);
     }
 }
